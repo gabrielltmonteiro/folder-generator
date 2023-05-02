e270a9aa14962c69dc760cbbfdb09c9782dbc5da
gabrielltmonteiro <gabrielltmonteiro@hotmail.com>
2023-05-01 22:28:49 -0300

    Gerador de pastas UTF-8 encoding

---
+++ pastas.bat
@@ -0,0 +1,8 @@
+@echo off
+CHCP 65001 
+md "AOO - Análise Orientada a Objetos" 
+md "RC - Redes de Computadores" 
+md  "AR - Administração de Redes" 
+md  "PADM - Programação de Aplicações para Dispositivos Móveis" 
+md  "PDJ - Programação de Desenvolvimento de Jogos" 
+md  "TCC I - Trabalho de Conclusão de Curso I" 
